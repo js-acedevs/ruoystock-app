c077902b72820a3c3200b3e267dc5dbdefc72dc8
Albrim Paqarizi <albrim@solution25.com>
2022-10-15 16:59:22 +0200

    Updated vite config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnvironment {
+    readonly NODE_ENV: string;
+    readonly REACT_APP_NAME: string;
+    readonly REACT_APP_PUBLIC_URL: string;
+    readonly REACT_APP_API_URI: string;
+    readonly REACT_APP_JWT_STORAGE_KEY: string;
+    // more env variables...
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    build: {
+        outDir: "build",
+        manifest: true,
+    },
+    publicDir: "public",
+    plugins: [
+        react(),
+        EnvironmentPlugin([
+            "NODE_ENV",
+            "REACT_APP_NAME",
+            "REACT_APP_PUBLIC_URL",
+            "REACT_APP_API_URI",
+            "REACT_APP_JWT_STORAGE_KEY",
+        ]),
+    ],
+});
